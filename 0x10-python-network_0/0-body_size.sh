363117319c8253c9d85f3839b35eda392f59faad
IsaacTranquilo <mutungaisaac76@gmail.com>
2024-01-05 20:17:36 +0300

    commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL using curl, and displays the size of the response body in bytes.
+# This script takes a URL as an argument and displays the size of the response body in bytes.
 curl -sI "$1" | grep -i "content-length" | awk '{print $2}' | tr -d '\r'
 
