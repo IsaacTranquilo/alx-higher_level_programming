e3654c0613839ea6546b15919a4d2bb6d8d016cd
IsaacTranquilo <mutungaisaac76@gmail.com>
2024-01-05 20:14:19 +0300

    commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-
-# Check if URL is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Extract the URL from the command line argument
-url=$1
-
-body_size=$(curl -sI "$url" | grep -i "content-length" | awk '{print $2}' | tr -d '\r')
-
-# Check if Content-Length header is present in the response
-if [ -z "$body_size" ]; then
-    echo "Unable to determine the size of the response body."
-    exit 1
-fi
-
-echo "Size of the response body: ${body_size} bytes"
+# This script takes a URL as an argument, sends a request to that URL using curl, and displays the size of the response body in bytes.
+curl -sI "$1" | grep -i "content-length" | awk '{print $2}' | tr -d '\r'
 
